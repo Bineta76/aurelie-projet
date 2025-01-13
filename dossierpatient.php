7e0af178e5746b58d9592375ddcf9d9eff82f0ed
Llily766 <au.binet21@gmail.com>
2025-01-13 09:27:39 +0100

    EPREUVE E5

---
+++ dossierpatient.php
@@ -0,0 +1,30 @@
+<?php
+include ("includes/header.php");
+?>
+   <div class="container">
+<h1>DossierPatient</h1>
+
+<h1>DOSSIERPATIENT</h1>
+<h3>Nom :</h3>
+<h3>Prénom :</h3>
+<h3>Photo</h3>
+<h3> Adresse du patient :</h3>  
+<h3> Date de naissance :</h3>   
+<h3> Sexe:</h3>
+<h3>Numero de sécurité sociale _ _ _ _ _ _ _ _ _ _ _ _ </h3>
+<h3>email</h3>
+<h3>téléphone</h3>
+<h3>Personne a contacter</h3>
+<h3>Médecin traitant</h3>
+<h3> Observations (Allergies,..)</h3>
+
+
+
+
+
+
+   </div>
+</body>
+</html>
+
+ 
---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "192.168.1.15";
+$username = "healthnorth";
+$password = "healthnorth-password";
+$dbname = "healthnorth";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
